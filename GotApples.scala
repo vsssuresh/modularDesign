59c455189905151ce0c4d06d79ace243dcd92a77
Suresh Velagapudi <suresh.b.velagapudi@gmail.com>
2012-06-12 11:35:40 -0400

    GotApples.scala modified for readability

--- GotApples.scala
+++ GotApples.scala
@@ -1,20 +1,17 @@
 package com.mayera.portfolio
 
 object GotApples {
-def main(args: Array[String]) {
-val db: Database =
-if(args(0) == "student")
-StudentDatabase
-else
-SimpleDatabase
+  def main(args: Array[String]) {
+    val db: Database =
+    if(args(0) == "student") StudentDatabase 
+    else SimpleDatabase
 
 
-object browser extends Browser {
-val database: db.type  = db
-}
-val apple = SimpleDatabase.CompanyNamed("Apple").get
-for(portfolio <- browser.
-portfoliosUsing(apple))
-println(portfolio)
-}
+    object browser extends Browser {
+        val database: db.type  = db
+    }
+    val apple = SimpleDatabase.CompanyNamed("Apple").get
+    for(portfolio <- browser.portfoliosUsing(apple))
+        println(portfolio)
+  }
 }
